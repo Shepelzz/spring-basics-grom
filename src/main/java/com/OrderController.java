9d822166601a69fbf28b9f17846913dd9078b6b3
Zhenya Shepel <shepelzz@gmail.com>
2018-11-05 16:40:23 +0200

    spring-2

---
+++ src/main/java/com/OrderController.java
@@ -0,0 +1,20 @@
+package com;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class OrderController {
+    @Autowired
+    private OrderService orderService;
+
+    @RequestMapping(method = RequestMethod.GET, value = "/orderSave", produces = "text/plan")
+    public @ResponseBody
+    String test(){
+        orderService.save(null);
+        return "ok";
+    }
+}
