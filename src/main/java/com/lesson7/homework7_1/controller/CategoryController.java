b285c732b4dc64d818d40367f318da3e6c5e428b
Zhenya Shepel <shepelzz@gmail.com>
2018-12-04 17:17:58 +0200

    spring-7 hw1

--- src/main/java/com/lesson7/homework7_1/controller/CategoryController.java
+++ src/main/java/com/lesson7/homework7_1/controller/CategoryController.java
@@ -1,11 +1,14 @@
 package com.lesson7.homework7_1.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.lesson7.homework7_1.dao.CategoryDAO;
 import com.lesson7.homework7_1.model.Category;
 import com.lesson7.homework7_1.service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+
+import java.io.InputStream;
 
 @Controller
 @RequestMapping(value = "/category")
@@ -20,11 +23,11 @@ public class CategoryController {
         this.categoryDAO = categoryDAO;
     }
 
-    @RequestMapping(method = RequestMethod.POST, value = "/save")
+    @RequestMapping(method = RequestMethod.POST, value = "/save", produces = "application/json")
     public @ResponseBody
-    Category save(@RequestBody Category category){
+    Category save(InputStream dataStream){
         try {
-            return categoryService.save(category);
+            return categoryService.save(new ObjectMapper().readValue(dataStream, Category.class));
         }catch (Exception e){
             e.printStackTrace();
             return null;
